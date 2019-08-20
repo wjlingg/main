9cfd54cc07422ea48ab5397c77eaaba4c4096c50
wjlingg <e0309488@u.nus.edu>
2019-08-20 19:01:23 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/GreetEchoExit.java
+++ src/main/java/GreetEchoExit.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class GreetEchoExit {
+    public static void main(String[] args) {
+        System.out.println(
+                "   ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "   ____________________________________________________________\n");
+
+        Scanner input = new Scanner(System.in);
+        String userString = input.next();
+
+        while(true){
+            if(!userString.equals("bye")){
+                System.out.println(
+                        "   ____________________________________________________________\n" +
+                        "     " + userString + "\n" +
+                        "   ____________________________________________________________\n"
+                );
+                userString = input.next();
+            }else{
+                System.out.println(
+                        "   ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "   ____________________________________________________________\n"
+                );
+                System.exit(1);
+            }
+        }
+    }
 }
