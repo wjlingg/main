3d533fdcf280fb1b73aeef671f0e7fe5c8c9651d
wjlingg <e0309488@u.nus.edu>
2019-08-21 12:05:03 +0800

    Level 1 and Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,107 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    private static final ArrayList<String> myList = new ArrayList<>();
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final char INPUT_COMMENT_MARKER = '#';
+
+    private static final String MESSAGE_BYE = "     Bye. Hope to see you again soon!\n";
+
+    private static final String COMMAND_GET_LIST = "list";
+    private static final String COMMAND_EXIT_PROGRAM = "bye";
+    private static final int DISPLAYED_INDEX_OFFSET = 1;
+    private static final String DIVIDER = "   ____________________________________________________________\n";
+
+
     public static void main(String[] args) {
+        showLogo();
+        showHelloMessage();
+        while (true) {
+            String userCommand = getUserInput();
+            executeCommand(userCommand);
+        }
+    }
+
+    private static void showLogo(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    private static void showHelloMessage() {
+        System.out.println(
+            DIVIDER +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            DIVIDER
+        );
+    }
+
+//    private static void echoUserCommand(String userCommand) {
+//        System.out.println(
+//            DIVIDER +
+//            "     " + userCommand + "\n" +
+//            DIVIDER
+//        );
+//    }
+
+    private static void executeCommand(String userInputString) {
+        if(userInputString.equals(COMMAND_GET_LIST)){
+            System.out.println(DIVIDER);
+            for (int i = 0; i < myList.size(); i++) {
+                final int displayIndex = i + DISPLAYED_INDEX_OFFSET;
+                System.out.println(
+                        "     " + displayIndex + ". " + myList.get(i)
+                );
+            }
+            System.out.println(DIVIDER);
+        }else if(userInputString.equals(COMMAND_EXIT_PROGRAM)){
+            System.out.println(DIVIDER + MESSAGE_BYE + DIVIDER);
+            System.exit(0);
+        }else{
+            System.out.println(
+                DIVIDER +
+                "     " + userInputString + "\n" +
+                DIVIDER
+            );
+        }
+//        switch (userInputString) {
+//            case COMMAND_GET_LIST:
+//                System.out.println(DIVIDER);
+//                for (int i = 0; i < myList.size(); i++) {
+//                    final int displayIndex = i + DISPLAYED_INDEX_OFFSET;
+//                    System.out.println(
+//                            "     " + displayIndex + ". " + myList.get(i)
+//                    );
+//                }
+//                System.out.println(DIVIDER);
+//            case COMMAND_EXIT_PROGRAM:
+//                System.out.println(DIVIDER + MESSAGE_BYE + DIVIDER);
+//                System.exit(0);
+//                // Fallthrough
+//            default:
+//                System.out.println(
+//                    DIVIDER +
+//                    "     " + userInputString + "\n" +
+//                    DIVIDER
+//                );
+//        }
+    }
+
+    private static String getUserInput() {
+        //System.out.print(LINE_PREFIX + "Enter command: ");
+        String inputLine = SCANNER.nextLine();
+        if (!inputLine.equals(COMMAND_GET_LIST)) {
+            myList.add(inputLine);
+        }
+        // silently consume all blank
+        while (inputLine.trim().isEmpty()) {
+            inputLine = SCANNER.nextLine();
+        }
+        return inputLine;
     }
 }
--- src/main/java/GreetEchoExit.java
+++ src/main/java/GreetEchoExit.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class GreetEchoExit {
     public static void main(String[] args) {
         System.out.println(
-                "   ____________________________________________________________\n" +
-                        "     Hello! I'm Duke\n" +
-                        "     What can I do for you?\n" +
-                        "   ____________________________________________________________\n");
+            "   ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "   ____________________________________________________________\n");
 
         Scanner input = new Scanner(System.in);
         String userString = input.next();
@@ -14,16 +14,16 @@ public class GreetEchoExit {
         while(true){
             if(!userString.equals("bye")){
                 System.out.println(
-                        "   ____________________________________________________________\n" +
-                                "     " + userString + "\n" +
-                                "   ____________________________________________________________\n"
+                    "   ____________________________________________________________\n" +
+                    "     " + userString + "\n" +
+                    "   ____________________________________________________________\n"
                 );
                 userString = input.next();
             }else{
                 System.out.println(
-                        "   ____________________________________________________________\n" +
-                                "     Bye. Hope to see you again soon!\n" +
-                                "   ____________________________________________________________\n"
+                    "   ____________________________________________________________\n" +
+                    "     Bye. Hope to see you again soon!\n" +
+                    "   ____________________________________________________________\n"
                 );
                 System.exit(1);
             }
